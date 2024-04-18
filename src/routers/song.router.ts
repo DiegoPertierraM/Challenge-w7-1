420aa0f596d53b0c7d67623f1238ed13cdd4fb8f
DiegoPertierraM <diegopmenendez@gmail.com>
2024-04-18 18:27:05 +0200

    Add users with register and login

--- src/routers/song.router.ts
+++ src/routers/song.router.ts
@@ -1,19 +1,35 @@
 import { Router as createRouter } from 'express';
 import createDebug from 'debug';
 import { type SongsController } from '../controllers/songs.controller.js';
+import { type AuthInterceptor } from '../middleware/auth.interceptor.js';
 
 const debug = createDebug('W7E:songs:router');
 
 export class SongsRouter {
   router = createRouter();
 
-  constructor(private readonly songsController: SongsController) {
+  constructor(
+    private readonly songsController: SongsController,
+    readonly authInterceptor: AuthInterceptor
+  ) {
     debug('Instantiated songs router');
 
     this.router.get('/', songsController.getAll.bind(songsController));
     this.router.get('/:id', songsController.getById.bind(songsController));
-    this.router.post('/', songsController.create.bind(songsController));
-    this.router.patch('/:id', songsController.update.bind(songsController));
-    this.router.delete('/:id', songsController.delete.bind(songsController));
+    this.router.post(
+      '/',
+      authInterceptor.authentication.bind(authInterceptor),
+      songsController.create.bind(songsController)
+    );
+    this.router.patch(
+      '/:id',
+      authInterceptor.authentication.bind(authInterceptor),
+      songsController.update.bind(songsController)
+    );
+    this.router.delete(
+      '/:id',
+      authInterceptor.authentication.bind(authInterceptor),
+      songsController.delete.bind(songsController)
+    );
   }
 }
